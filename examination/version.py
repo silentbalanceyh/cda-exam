6a672d8ab9e25707d7a0ee66bb184163875a1914
silentbalanceyh <silentbalanceyh@126.com>
2024-02-18 14:53:47 +0800

    Exam

--- examination/version.py
+++ examination/version.py
@@ -1,2 +1,17 @@
 import xgboost as xgb
-print(xgb.__version__)
+import catboost as cat
+import lightgbm as gbm
+import sklearn as sk
+import imblearn as imb
+import gensim as gsm
+import pandas as pd
+import numpy as np
+
+print("xgboost -> " + xgb.__version__)
+print("catboost -> " + cat.__version__)
+print("lightgbm -> " + gbm.__version__)
+print("sklearn -> " + sk.__version__)
+print("imblearn -> " + imb.__version__)
+print("gensim -> " + gsm.__version__)
+print("pandas -> " + pd.__version__)
+print("numpy -> " + np.__version__)
---
+++ version.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+python examination/version.py
